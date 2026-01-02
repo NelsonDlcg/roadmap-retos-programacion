4d5c651f860646193a18943e1724732a4850625c
NelsonDlcg <114178184+NelsonDlcg@users.noreply.github.com>
2026-01-02 13:00:42 -0500

    Create NelsonDlcg.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/NelsonDlcg.py
@@ -0,0 +1,37 @@
+# https://www.python.org/
+
+# Comentario en una linea
+
+"""
+También este es 
+un comentario 
+en varias líneas
+"""
+
+'''
+También este es 
+un comentario 
+en varias líneas
+'''
+
+my_variable = "Mi Variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi constante" #por convención
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_boll = False
+my_string = "Mi cadena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola, Phyton!")
+
+print(Type(my_int))
+print(Type(my_float))
+print(Type(my_bool))
+print(Type(my_string))
+print(Type(my_other_string))
+
+
